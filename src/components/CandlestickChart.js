3c13f32f84a3e2b721e8e1f1057832b1049658c1
Krysto <quy.p.hoang@gmail.com>
2022-04-19 23:22:38 +0700

    Candlestick chart colors compatible with the panel

--- src/components/CandlestickChart.js
+++ src/components/CandlestickChart.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from '@emotion/styled';
+import { useTheme } from '@emotion/react';
 import { createChart } from 'lightweight-charts';
 
 import { refreshCandles } from 'actions/market';
@@ -15,6 +16,7 @@ export default function CandlestickChart({ pairID }) {
   const seriesRef = useRef();
   const dispatch = useDispatch();
   const data = useSelector((state) => state.ui.market.candlestick[pairID]);
+  const theme = useTheme();
 
   useEffect(() => {
     // TODO: Batch
@@ -23,7 +25,19 @@ export default function CandlestickChart({ pairID }) {
 
   useEffect(() => {
     if (data && !chartRef.current) {
-      const chart = createChart(containerRef.current);
+      const chart = createChart(
+        containerRef.current,
+        theme
+          ? {
+              layout: {
+                background: {
+                  color: theme.lower(theme.background, 0.3),
+                },
+                textColor: theme.mixer(0.75),
+              },
+            }
+          : undefined
+      );
       seriesRef.current = chart.addCandlestickSeries();
       seriesRef.current.setData(data);
       chartRef.current = chart;
