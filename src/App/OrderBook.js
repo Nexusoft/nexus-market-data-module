5a9f86e6b45c4b779d573dc95f15015aa4d695d3
Krysto <quy.p.hoang@gmail.com>
2022-05-10 16:52:58 +0700

    using depth chart in OrderBook tab

--- src/App/OrderBook.js
+++ src/App/OrderBook.js
@@ -1,81 +1,6 @@
-import { useEffect } from 'react';
-import styled from '@emotion/styled';
-import { useSelector, useDispatch } from 'react-redux';
-
 import TradingPair from 'components/TradingPair';
-import { tradingPairIDs, tradingPairs } from 'constants';
-import { refreshOrderBook } from 'actions/market';
-
-const OrderBookWrapper = styled.div({
-  display: 'grid',
-  gridTemplateColumns: '1fr 1fr',
-  columnGap: '2em',
-});
-
-const OrderList = styled.div(({ theme }) => ({
-  display: 'flex',
-  justifyContent: 'space-between',
-}));
-
-const Cell = styled.div({
-  whiteSpace: 'nowrap',
-  overflow: 'hidden',
-  padding: '.2em 0',
-  textAlign: 'right',
-});
-
-const HeaderCell = styled(Cell)(({ align = 'left' }) => ({
-  opacity: 0.67,
-  textAlign: align,
-}));
-
-function PairOrderBook({ pairID }) {
-  const { baseTicker, quoteTicker } = tradingPairs[pairID];
-  const orderBook = useSelector((state) => state.ui.market.orderBook[pairID]);
-  const { asks, bids } = orderBook || {};
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    // TODO: Batch
-    dispatch(refreshOrderBook(pairID));
-  }, []);
-
-  return (
-    !!orderBook && (
-      <OrderBookWrapper>
-        <OrderList>
-          <div>
-            <HeaderCell>Price</HeaderCell>
-            {bids.map(({ price }, i) => (
-              <Cell key={i}>{price}</Cell>
-            ))}
-          </div>
-          <div>
-            <HeaderCell align="right">Quantity ({baseTicker})</HeaderCell>
-            {bids.map(({ quantity }, i) => (
-              <Cell key={i}>{quantity}</Cell>
-            ))}
-          </div>
-        </OrderList>
-
-        <OrderList>
-          <div>
-            <HeaderCell>Price</HeaderCell>
-            {asks.map(({ price }, i) => (
-              <Cell key={i}>{price}</Cell>
-            ))}
-          </div>
-          <div>
-            <HeaderCell align="right">Quantity ({baseTicker})</HeaderCell>
-            {asks.map(({ quantity }, i) => (
-              <Cell key={i}>{quantity}</Cell>
-            ))}
-          </div>
-        </OrderList>
-      </OrderBookWrapper>
-    )
-  );
-}
+import DepthChart from 'components/DepthChart';
+import { tradingPairIDs } from 'constants';
 
 export default function OrderBook() {
   return (
@@ -86,7 +11,7 @@ export default function OrderBook() {
             <TradingPair pairID={pairID} />
           </div>
           <div className="mt1">
-            <PairOrderBook pairID={pairID} />
+            <DepthChart pairID={pairID} />
           </div>
         </div>
       ))}
